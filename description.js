7d875bb9f84049eaaed338200d67ed0ff13d1678
Scorpio Anonymous <41952210+firefoxdidong@users.noreply.github.com>
2020-12-07 14:31:34 +0700

    Update

--- description.js
+++ description.js
@@ -143,7 +143,7 @@ function loadRecentUpdates()
 	var count = 0;
 	for (var dicNow in allPackages) {
 		count++;
-		if(count > 5) {
+		if(count > 10) {
 			break;
 		}
 		var urlOpen = "cydia://package/"+allPackages[dicNow].package;
